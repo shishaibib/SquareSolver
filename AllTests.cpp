80e9801548e7ae7902c949ec16d6f906cd55498a
AleksandrSivchuk <sashasivchuk12@gmail.com>
2024-08-24 13:52:02 +0300

    Add Tests

--- AllTests.cpp
+++ AllTests.cpp
@@ -18,11 +18,13 @@ struct TestData data[] = {
                              {1, 1, 0, -4, 2, -2, TWO_ROOTS},
                              {2, 0, 0, 0, 0, 0, INF_ROOTS},
                              {3, 0, 2, -4, 2, 0, ONE_ROOT},
-                             {4, 1, 0, 1, 0, 0, NO_ROOTS}
+                             {4, 1, 0, 1, 0, 0, NO_ROOTS},
+                             {5, 1, -2, 1, 1, 1, ONE_ROOT},
+                             {6, 0, 0, 2, 0, 0, NO_ROOTS}
                          };
 int RunTests()
 {
-    for(int i = 0; i <= 3; i++)
+    for(int i = 0; i <= 5; i++)
         {
             double a = data[i].a;
             double b = data[i].b;
