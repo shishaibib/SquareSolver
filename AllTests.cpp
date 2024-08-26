cbed1c31a55212c6aa607df49fbe8e00bdccaa89
AleksandrSivchuk <sashasivchuk12@gmail.com>
2024-08-26 21:43:07 +0300

    corrected codstyle

--- AllTests.cpp
+++ AllTests.cpp
@@ -12,20 +12,22 @@ int RunTests()
     int ScanTest = fscanf (testsdata,"%d %lg %lg %lg %lg %lg %d\n", &nTest, &a, &b, &c, &x1e, &x2e, &N_rootsE);
     while(ScanTest == 7)
     {
+
             double x1=0,x2=0;
             int N_roots = Square_Solve (a, b, c, &x1, &x2);
 
             if(N_roots != N_rootsE || IsEqual(x1, x1e) || IsEqual(x2, x2e))
-                {
-                    printf("ErrorTest %d, a = %lg, b = %lg, c = %lg,"
-                           "x1 = %lg, x2 = %lg,N_roots = %d,"
-                           "x1e = %lg,x2e = %lg,N_rootsE = %d",
-                           nTest, a, b, c, x1, x2, N_roots,
-                           x1e, x2e, N_rootsE);
-                    return 1;
-                }
-             printf("test %d passed\n",nTest);
-             ScanTest = fscanf (testsdata,"%d %lg %lg %lg %lg %lg %d\n", &nTest, &a, &b, &c, &x1e, &x2e, &N_rootsE);
+            {
+                printf("ErrorTest %d, a = %lg, b = %lg, c = %lg,"
+                       "x1 = %lg, x2 = %lg,N_roots = %d,"
+                       "x1e = %lg,x2e = %lg,N_rootsE = %d",
+                       nTest, a, b, c, x1, x2, N_roots,
+                       x1e, x2e, N_rootsE);
+                return 1;
+            }
+
+            printf("test %d passed\n",nTest);
+            ScanTest = fscanf (testsdata,"%d %lg %lg %lg %lg %lg %d\n", &nTest, &a, &b, &c, &x1e, &x2e, &N_rootsE);
 
     }
 }
